ea8f9ccd3f93de93ad0eda210f0219b8e51aee92
Tombo <Tombo.streaming@gmail.com>
2021-06-16 18:00:39 +0200

    Nachricht beim Joinen komplett überarbeitet

--- commands/setfirstjoin.js
+++ commands/setfirstjoin.js
@@ -1,7 +1,10 @@
 const mongo = require('../function/mongo')
 const command = require('./command')
 const english = require('../languages/english.json')
-const fjtitelschema = require('../schema/fj-titel-schema')
+const fjSchema = require('../schema/fj-schema')
+let Discord = require('discord.js');
+
+const cache = {}
 
 module.exports = (bot) => {
 
@@ -28,23 +31,36 @@ module.exports = (bot) => {
         for(var i = 0; i<split.length; i++){
             titel +=" "+ split[i]
         }
+        /////// 
+        ///////
+        /////// The clear function still in work
+        ///////
+        ///////
+        if(titel == 'clear'){
+            var titel = ""
+            console.log("Only clear");
+            console.log(titel)
+        }else{
         console.log(titel)
+
+        //cache[guild.id] = [titel]
 
         await mongo().then(async (mongoose) => {
             try {
-                await fjtitelschema.findOneAndUpdate({
-                    _id: guild._id
+                await fjSchema.findOneAndUpdate({
+                    _id: guild.id
                 }, {
                     _id: guild.id,
-                    text: titel,
+                    titel: titel,
                 }, {
                     upsert: true
                 })
             } finally {
-                mongoose.connection.close()
             }
         })
+
         channel.send('Titel successfully set')
+    }
     })
 
     command(bot, 'firstjoin color', async (message) => {
@@ -71,21 +87,22 @@ module.exports = (bot) => {
             color +=" "+ split[i]
         }
         console.log(color)
+        //cache[guild.id] = [color]
 
         await mongo().then(async (mongoose) => {
             try {
-                await fjcolorschema.findOneAndUpdate({
-                    _id: guild._id
+                await fjSchema.findOneAndUpdate({
+                    _id: guild.id
                 }, {
                     _id: guild.id,
-                    text: color,
+                    color: color,
                 }, {
                     upsert: true
                 })
             } finally {
-                mongoose.connection.close()
             }
         })
+
         channel.send('Color successfully set')
     })
 
@@ -113,22 +130,138 @@ module.exports = (bot) => {
             description +=" "+ split[i]
         }
         console.log(description)
+        //cache[guild.id] = [description]
 
         await mongo().then(async (mongoose) => {
             try {
-                await fjdescriptionschema.findOneAndUpdate({
-                    _id: guild._id
+                await fjSchema.findOneAndUpdate({
+                    _id: guild.id
                 }, {
                     _id: guild.id,
-                    text: description,
+                    description: description,
                 }, {
                     upsert: true
                 })
             } finally {
-                mongoose.connection.close()
             }
         })
         channel.send('Description successfully set')
     })
 
-}
+    //Deactivate
+    command(bot, 'firstjoin deactivate', async (message) => {
+        const { member, channel, content, guild } = message
+
+        if (!member.hasPermission('ADMINISTRATOR')) {
+            channel.send(`${english.not_allowed}`)
+            return;
+        }
+
+        let text = content
+
+        const split = text.split(' ')
+
+        if (split.length != 2) {
+            channel.send('Something went wrong.')
+            return;
+        }
+
+        var toggle = new Boolean("")
+        //cache[guild.id] = [toggle]
+
+        await mongo().then(async (mongoose) => {
+            try {
+                await fjSchema.findOneAndUpdate({
+                    _id: guild.id
+                }, {
+                    _id: guild.id,
+                    toggle: toggle,
+                }, {
+                    upsert: true
+                })
+            } finally {
+            }
+        })
+        channel.send('Join PM deactivated')
+    })
+
+        //Activate
+    command(bot, 'firstjoin activate', async (message) => {
+        const { member, channel, content, guild } = message
+    
+        if (!member.hasPermission('ADMINISTRATOR')) {
+            channel.send(`${english.not_allowed}`)
+            return;
+        }
+    
+        let text = content
+    
+        const split = text.split(' ')
+    
+        if (split.length != 2) {
+            channel.send('Something went wrong.')
+            return;
+        }
+    
+        var toggle = new Boolean(true)
+        //cache[guild.id] = [toggle]
+    
+        await mongo().then(async (mongoose) => {
+            try {
+                await fjSchema.findOneAndUpdate({
+                    _id: guild.id
+                }, {
+                    _id: guild.id,
+                    toggle: toggle,
+                }, {
+                    upsert: true
+                })
+            } finally {
+            }
+        })
+        channel.send('Join PM activated')
+    })
+
+     const onJoin = async member => {
+            const { guild } = member
+        
+           // let data = cache[guild.id]
+            
+            //if(!data){
+                console.log('Fetching First Join from DATABASE')
+        
+                await mongo().then(async (mongoose) => {
+                    try {
+                        result2 = await fjSchema.findOne({_id: guild.id})
+
+                        //console.log(result2)
+                        if(result2 == null)return;
+                        console.log("Caching first join from data")
+                        cache[guild.id] = data = [result2.titel, result2.color, result2.description, result2.toggle]
+                    } finally {
+                    }
+                })
+           //}
+            if(result2 == null)return;
+        
+            const titel = data[0]
+            const color = data[1]
+            const description = data[2]
+            const toggle = data[3]
+        
+            console.log(color)
+
+            if(toggle == false)return;
+        
+            let embed = new Discord.MessageEmbed()
+            .setAuthor(titel, bot.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+            .setThumbnail(member.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+            .setColor(color)
+            .setDescription(description)
+            member.send(embed);
+        };
+
+        bot.on('guildMemberAdd', member => {
+            onJoin(member)
+        })
+    }
