25f3e004796e7e8bef2faf6b1b451935221956c5
Tombo <Tombo.streaming@gmail.com>
2021-06-16 18:02:48 +0200

    In Arbeit 👀

---
+++ function/selfrole.js
@@ -0,0 +1,8 @@
+/*This function will rightknow only be available for my Discord server.
+You can just change the code, if you want to use it also for your server.
+*/
+module.exports = (bot) => {
+const channelid = d;
+
+    
+}
